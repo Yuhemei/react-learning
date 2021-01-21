ef87ded1a1176420e3ee58198e495608a8e089d6
yuyongjin <yuyongjin@antiy.cn>
2021-01-21 16:30:53 +0800

    加快react app的编译速度

--- src/index.js
+++ src/index.js
@@ -39,9 +39,9 @@ class Board extends React.Component {
         }
         squares[i] = this.state.xIsnext ? 'X' : 'O';
         this.setState({
-                squares: squares,
-                xIsnext: !this.state.xIsnext
-            });
+            squares: squares,
+            xIsnext: !this.state.xIsnext
+        });
     }
     renderSquare(i) {
         return (
@@ -84,6 +84,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
     render() {
         return (
             <div className="game">
